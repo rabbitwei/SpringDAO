69fd68a532fbdc5dc2a6bf7fe9932f2c2b04b1e0
rabbitwei <rabbitwei1999@163.com>
2019-10-24 16:15:51 +0800

    spring 使用 jdbc 来进行 dao 操作，使用 注解的方式

--- src/main/java/cn/rabbit/service/UserService.java
+++ src/main/java/cn/rabbit/service/UserService.java
@@ -3,12 +3,16 @@ package cn.rabbit.service;
 import cn.rabbit.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
     @Autowired
     private UserDAO userDAO;
 
+    //想要控制哪个方法事务，在其前面添加@Transactional这个注解就行了
+    //如果想要控制整个类的事务，那么在类上面添加就行了。
+    @Transactional
     public void add() {
         userDAO.add();
         int i = 1/ 0;
